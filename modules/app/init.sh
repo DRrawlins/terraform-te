958ac93dd96c902cee06c6b90e9185116507dee0
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-17 15:21:18 -0500

    updated cloud init it now deploys properly

--- modules/app/init.sh
+++ modules/app/init.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -x
+export HOME=/root
 
-git clone https://github.com/DRrawlins/chef_role_te.git
 sudo dnf install -y ruby ruby-devel
 gem install bundle
-cd chef_role_te
-./install-fed-dependencies.sh
-bundle exec berks install
-bundle exec berks vendor
+cd /root
+git clone https://github.com/DRrawlins/chef_role_te.git
+cd /root/chef_role_te
+bundle install --system
+berks install
+berks vendor
 mv berks-cookbooks/ cookbooks
-sudo bundle install
-sudo bundle exec chef-client -z -o chef_role_te
+chef-client -z -o chef_role_te::default
 sudo iptables --flush
